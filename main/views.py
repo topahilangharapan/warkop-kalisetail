ca88ac433ab25cd5b6ec779612e3880781296f85
topahilangharapan <musthofa.joko@ui.ac.id>
2023-09-09 14:24:21 +0700

    update: menyesuaikan dengan ketentuan soal

--- main/views.py
+++ main/views.py
@@ -3,11 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Kopi Hitam w/ Ampas' ,
-        'amount': 99 ,
-        'description': 'Kopi hitam nikmat yang membantu Anda untuk menjalani seluruh kegiatan ' \
-                       'sehari-hari ditambah ampas-nya untuk membantu pencernaan',
-        'price': 4000 ,
+        'nama': 'Musthofa Joko Anggoro' ,
+        'kelas': 'PBP E',
     }
 
     return render(request, "main.html", context)
