f712eef955e727ae34e7d333264155b616043994
topahilangharapan <musthofa.joko@ui.ac.id>
2023-09-11 13:33:59 +0700

    update: menambah appname

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'appname': 'Warkop Kalisetail',
         'nama': 'Musthofa Joko Anggoro' ,
         'kelas': 'PBP E',
     }
