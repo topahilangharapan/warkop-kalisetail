a2909b58abc1d797cfe5bbc31ccfb5ff824d4bbb
topahilangharapan <musthofa.joko@ui.ac.id>
2023-09-18 16:23:57 +0700

    add: tugas 3

--- warkop_kalisetail/settings.py
+++ warkop_kalisetail/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'warkop_kalisetail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
