6bffd6b7d486c8f2d98553575e372c6fc623dadd
topahilangharapan <musthofa.joko@ui.ac.id>
2023-09-19 23:00:42 +0700

    fixed: urls.py tertukar

--- warkop_kalisetail/urls.py
+++ warkop_kalisetail/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for shopping_list project.
+URL configuration for warkop_kalisetail project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,14 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'),
-    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
 
 ]
