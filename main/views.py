6a04f9d0216e7ef9f70bf9476a32a2f0b427fb7d
topahilangharapan <musthofa.joko@ui.ac.id>
2023-11-20 23:51:01 +0700

    add:tugas9

--- main/views.py
+++ main/views.py
@@ -12,7 +12,9 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 import datetime
+import json
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -138,3 +140,23 @@ def delete_product_ajax(request, id):
     product.delete()
     response = HttpResponseRedirect(reverse("main:show_main"))
     return response
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            amount = data["amount"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
